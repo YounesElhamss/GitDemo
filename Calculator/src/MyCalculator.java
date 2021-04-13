22c4c3ae976e4c26340c397509c89d9b3f5c1d72
Younes Elhamss <YounesElhamss@Youness-MacBook.local>
2021-04-13 10:47:01 +0200

    add calc

---
+++ Calculator/src/MyCalculator.java
@@ -0,0 +1,8 @@
+public class MyCalculator {
+    public static void main(String[] args) {
+
+    }
+    public static boolean intSum(int a, int b) {
+        return  a < b;
+    }
+}
---
+++ Calculator/test/MyCalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyCalculatorTest {
+
+    @Test
+    public void intSum() {
+       // boolean result = MyCalculator.intSum(3,4);
+
+        //int expected = 7;
+        assertFalse(false);
+    }
+}
